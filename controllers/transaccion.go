d9e998085616951742ff4e7ecc905b6a02035a0c
Israel Gonzalez <criptopreto@gmail.com>
2024-04-16 01:34:14 -0400

    Debug

--- controllers/transaccion.go
+++ controllers/transaccion.go
@@ -1,14 +1,13 @@
 package controllers
 
 import (
-    
-    "net/http"
-    "github.com/gin-gonic/gin"
-    "banking/config" // Asegúrate de que este es el nombre correcto de tu paquete de configuración
-    "banking/models"
+	configs "banking/config" // Asegúrate de que este es el nombre correcto de tu paquete de configuración
+	"banking/models"
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
-
-
 
 // ProcesarTransaccion godoc
 // @Summary Procesa una transacción entre un usuario y un comercio
@@ -23,71 +22,70 @@ import (
 // @Failure 500 {object} map[string]interface{}
 // @Router /procesar-transaccion [post]
 func ProcesarTransaccion(c *gin.Context) {
-    var input models.TransaccionInput
+	var input models.TransaccionInput
 
-    // Intenta vincular el JSON de entrada a la estructura TransaccionInput
-    if err := c.ShouldBindJSON(&input); err != nil {
-        c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-        return
-    }
+	// Intenta vincular el JSON de entrada a la estructura TransaccionInput
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
 
-    usuarioCedula := input.Usuario.Cedula
-    comercioRif := input.Comercio.Rif
-    montoTransaccion := float32(input.Comercio.Monto)
+	fmt.Println("Input")
+	fmt.Println(input)
 
-    var usuarioDB, comercioDB models.User
-    resultUser := configs.DB.Where("cedula = ?", usuarioCedula).First(&usuarioDB)
-    resultComercio := configs.DB.Where("rif = ?", comercioRif).First(&comercioDB)
+	usuarioCedula := input.Usuario.Cedula
+	comercioRif := input.Comercio.Rif
+	montoTransaccion := float32(input.Comercio.Monto)
 
-    // Verifica errores en la búsqueda
-    if resultUser.Error != nil {
-        c.JSON(http.StatusNotFound, gin.H{"error": "Usuario no encontrado"})
-        return
-    }
-    if resultComercio.Error != nil {
-        c.JSON(http.StatusNotFound, gin.H{"error": "Comercio no encontrado"})
-        return
-    }
+	var usuarioDB, comercioDB models.User
+	resultUser := configs.DB.Where("cedula = ?", usuarioCedula).First(&usuarioDB)
+	resultComercio := configs.DB.Where("rif = ?", comercioRif).First(&comercioDB)
 
-    // Verifica si el usuario tiene saldo suficiente
-    if usuarioDB.Saldo < montoTransaccion {
-        c.JSON(http.StatusBadRequest, gin.H{"error": "Saldo insuficiente"})
-        return
-    }
+	// Verifica errores en la búsqueda
+	if resultUser.Error != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Usuario no encontrado"})
+		return
+	}
+	if resultComercio.Error != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Comercio no encontrado"})
+		return
+	}
 
-   
-    usuarioDB.Saldo -= montoTransaccion
-  
+	// Verifica si el usuario tiene saldo suficiente
+	if usuarioDB.Saldo < montoTransaccion {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Saldo insuficiente"})
+		return
+	}
 
-  
-    comercioDB.Saldo += montoTransaccion
-   
+	usuarioDB.Saldo -= montoTransaccion
 
-    // Actualiza ambos registros dentro de una transacción para asegurar la atomicidad
-    tx := configs.DB.Begin()
-    if err := tx.Save(&usuarioDB).Error; err != nil {
-        tx.Rollback()
-        c.JSON(http.StatusInternalServerError, gin.H{"error": "No se pudo debitar el saldo del usuario"})
-        return
-    }
+	comercioDB.Saldo += montoTransaccion
 
-    if err := tx.Save(&comercioDB).Error; err != nil {
-        tx.Rollback()
-        c.JSON(http.StatusInternalServerError, gin.H{"error": "No se pudo acreditar el saldo al comercio"})
-        return
-    }
+	// Actualiza ambos registros dentro de una transacción para asegurar la atomicidad
+	tx := configs.DB.Begin()
+	if err := tx.Save(&usuarioDB).Error; err != nil {
+		tx.Rollback()
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "No se pudo debitar el saldo del usuario"})
+		return
+	}
 
-    // Confirma los cambios si todo sale bien
-    tx.Commit()
+	if err := tx.Save(&comercioDB).Error; err != nil {
+		tx.Rollback()
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "No se pudo acreditar el saldo al comercio"})
+		return
+	}
 
-    // Antes de responder, establece los datos actualizados en el contexto para el middleware
-    c.Set("usuario", usuarioDB)
-    c.Set("comercio", comercioDB)
+	// Confirma los cambios si todo sale bien
+	tx.Commit()
 
-    // Responde con la confirmación de la transacción
-    c.JSON(http.StatusOK, gin.H{
-        "message": "Transacción exitosa",
-        "usuarioSaldoActual": usuarioDB.Saldo,
-        "comercioSaldoActual": comercioDB.Saldo,
-    })
+	// Antes de responder, establece los datos actualizados en el contexto para el middleware
+	c.Set("usuario", usuarioDB)
+	c.Set("comercio", comercioDB)
+
+	// Responde con la confirmación de la transacción
+	c.JSON(http.StatusOK, gin.H{
+		"message":             "Transacción exitosa",
+		"usuarioSaldoActual":  usuarioDB.Saldo,
+		"comercioSaldoActual": comercioDB.Saldo,
+	})
 }
